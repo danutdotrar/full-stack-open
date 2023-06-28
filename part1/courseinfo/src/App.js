2abc0e2194abbc520d104f796fc683cd1c3d6940
danutdotrar <morarasu.danut@gmail.com>
2023-06-28 17:24:41 +0300

    Add step1 solving

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,49 @@
+const Header = (prop) => {
+    return (
+        <>
+            <h1>{prop.course}</h1>
+        </>
+    );
+};
+
+const Content = (prop) => {
+    return (
+        <>
+            <p>
+                {prop.part} {prop.exercise}
+            </p>
+        </>
+    );
+};
+
+const Total = (prop) => {
+    return (
+        <>
+            <p>Number of exercises {prop.exercise}</p>
+        </>
+    );
+};
+
+const App = () => {
+    const course = "Half Stack application development";
+    const part1 = "Fundamentals of React";
+    const exercises1 = 10;
+    const part2 = "Using props to pass data";
+    const exercises2 = 7;
+    const part3 = "State of a component";
+    const exercises3 = 13;
+
+    return (
+        <div>
+            <Header course={course} />
+
+            <Content part={part1} exercise={exercises1} />
+            <Content part={part2} exercise={exercises2} />
+            <Content part={part3} exercise={exercises3} />
+
+            <Total exercise={exercises1 + exercises2 + exercises3} />
+        </div>
+    );
+};
+
+export default App;

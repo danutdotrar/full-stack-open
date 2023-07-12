d16c7410cf8f96f0aae834a0164e0369df23fc74
danutdotrar <morarasu.danut@gmail.com>
2023-07-12 14:16:40 +0300

    Add fly and render service

--- part3/coursedata/index.js
+++ part3/coursedata/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const http = require("http");
+
+app.use(express.static("build"));
+app.use(cors());
+app.use(express.json());
+// const http = require("http");
 
 let notes = [
     {
@@ -26,13 +31,54 @@ let notes = [
 // });
 
 app.get("/", (request, response) => {
-    response.send("<h1>Hello world!<h1/>");
+    response.json(notes);
 });
 
 app.get("/api/notes", (request, response) => {
     response.json(notes);
 });
 
-const PORT = 3001;
+app.get("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find((note) => note.id === id);
+
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id);
+    notes = notes.filter((note) => note.id !== id);
+
+    response.status(204).end();
+});
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+    return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+    const body = request.body;
+
+    if (!body.content) {
+        return response.status(400).json({ error: "content missing" });
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    };
+
+    notes = notes.concat(note);
+
+    response.json(note);
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);

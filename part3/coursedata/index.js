9b6ce41d9ff0c916e6b4052a4f01f117731c5065
danutdotrar <morarasu.danut@gmail.com>
2023-07-17 11:46:43 +0300

    Modify error handling

--- part3/coursedata/index.js
+++ part3/coursedata/index.js
@@ -48,7 +48,7 @@ app.delete("/api/notes/:id", (request, response, next) => {
 //     return maxId + 1;
 // };
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
     const body = request.body;
 
     if (body.content === undefined) {
@@ -60,20 +60,26 @@ app.post("/api/notes", (request, response) => {
         important: body.important || false,
     });
 
-    note.save().then((savedNote) => {
-        response.json(savedNote);
-    });
+    note.save()
+        .then((savedNote) => {
+            response.json(savedNote);
+        })
+        .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
-    const body = request.body;
+    const { content, important } = request.body;
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    };
+    // const note = {
+    //     content: body.content,
+    //     important: body.important,
+    // };
 
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: "query" }
+    )
         .then((updatedNote) => {
             response.json(updatedNote);
         })
@@ -85,6 +91,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError") {
         return response.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error);

9074c57b141549a4081974b48574764bd246b0a8
danutdotrar <morarasu.danut@gmail.com>
2023-07-19 13:01:22 +0300

    Change port to process.env.PORT

--- part4/bloglist/backend/index.js
+++ part4/bloglist/backend/index.js
@@ -128,7 +128,7 @@ app.post("/api/blogs", (request, response) => {
 });
 
 // PORT listening
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
